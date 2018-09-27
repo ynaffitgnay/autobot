11a85403b2c64e885b4ab93b21cb192eae3992ed
Rahul Moghe <rahul.moghe@utexas.edu>
2018-09-27 16:35:12 -0500

    face the ball PID behavior done

--- core/python/behaviors/walk_to_ball_kick.py
+++ core/python/behaviors/walk_to_ball_kick.py
@@ -5,6 +5,8 @@ from __future__ import division
 from __future__ import absolute_import
 
 import memory
+import time
+import numpy as np
 import math
 import core
 import pose
@@ -46,12 +48,13 @@ class AlignBallGoal(Event):
     self.goal = goal
 
   def ready():
-    return aligned_boolean
+    return ((abs(ball.visionBearing-goal.visionBearing) < 0.1) and (abs(core.joint_values[core.HeadYaw]) < 0.1))
 
 def A(ball = None, goal = None):
   """Ball, Goal, Robot aligned"""
   return AlignBallGoal(ball, goal)
 
+"""I don't think we need this because page 1 of the assignment says it will be manually moved to about 2 meters away"""
 class ScoredGoal(Event):
   """Aligning ball, goal and robot"""
   def __init__(self, ball, goal):
@@ -69,23 +72,23 @@ def S(ball = None, goal = None):
 class GetReady(Node):
   def run(self):
     commands.stand()
-    commands.setHeadTilt(-30.0)
+    commands.setHeadTilt(-10.0)
     if self.getTime() > 1.5:
       self.finish()
 
 class MoveHeadLeft(Node):
   """Search for the ball to the left"""
   def run(self):
-    commands.setHeadPanTilt(-math.pi/2,-30.0,1.5)
-    commands.setHeadTilt(-30.0)
+    commands.setHeadPanTilt(core.DEG_T_RAD*110.0,-15.0,1.5)
+    commands.setHeadTilt(-15.0)
     if self.getTime() > 2.5:
       self.finish()
 
 class MoveHeadRight(Node):
   """Search for the ball to the right"""
   def run(self):
-    commands.setHeadPanTilt(math.pi/2,-30.0,1.5)
-    commands.setHeadTilt(-30.0)
+    commands.setHeadPanTilt(-core.DEG_T_RAD*110.0,-15.0,1.5)
+    commands.setHeadTilt(-15.0)
     if self.getTime() > 2.5:
       self.finish()
 
@@ -94,29 +97,61 @@ class FaceBall(Node):
   def __init__(self, ball):
     super(FaceBall, self).__init__()
     self.ball = ball
+    self.kP = 0.1
+    self.kI = 0.01
+    self.kD = 0.01
+    self.theta_integral = 0.0
+    self.theta_prev = 0.0
+    self.time_last = time.clock()
+    self.time_current = time.clock()
+
+  def calc_theta_integral(self):
+    self.theta_integral = self.theta_integral + (self.time_current - self.time_last)*(self.ball.visionBearing)
+    if abs(self.theta_integral) >= 15:
+      self.theta_integral = 15*np.sign(self.theta_integral)
+
+  def run(self):
+    self.time_current = time.clock()
+    self.calc_theta_integral()
+    bearing = self.ball.visionBearing
+    commands.setHeadPanTilt(bearing, -15.0, 0.1)
+    if abs(self.ball.visionBearing) > 0.1:
+      commands.setWalkVelocity(0.0, 0.0, 0.5*(np.sign(self.ball.visionBearing)))
+    else:
+      theta_cont = self.kP * bearing + self.kI * self.theta_integral + self.kD *(bearing - self.theta_prev) / (self.time_current - self.time_last)
+      print("Controller input: %.5f, Bearing: %.5f, Integral: %.5f, Difference: %.5f, Delta T: %.5f\n" % (theta_cont, bearing, self.theta_integral, (bearing - self.theta_prev), (self.time_current - self.time_last)))
+      commands.setWalkVelocity(0.0, 0.0, 0.0001*theta_cont)
+    self.theta_prev = self.ball.visionBearing
+    self.time_last = self.time_current
 
 class WalkToBall(Node):
   """Controller node for tracking the ball"""
-  integral = 0.0
-  def __init__(self, ball, kP = 0.0, kI = 0.0, kD = 0.0):
+  def __init__(self, ball):
     super(WalkToBall, self).__init__()
     self.ball = ball
-    self.kP = kP
-    self.kI = kI
-    self.kD = kD
+    self.kP = 1.0
+    self.kI = 1.0
+    self.kD = 1.0
+    self.integral = 0.0
+    self.dist_prev = 0.0
 
   def calc_integral(self):
-    WalkToBall.integral = WalkToBall.integral + self.ball.visionDistance
+    self.integral = self.integral + self.ball.visionDistance
 
   def run(self):
-    ball = memory.world_objects.getObjPtr(core.WO_BALL)
-    bearing = ball.visionBearing
-    elevation = core.RAD_T_DEG * ball.visionElevation
-    distance = ball.visionDistance
+    bearing = self.ball.visionBearing
+    elevation = core.RAD_T_DEG * self.ball.visionElevation
+    distance = self.ball.visionDistance
     self.calc_integral()
-    # print('Ball!\t Bearing: %f \t Distance: %f\t Elevation: %.8f \t Integral: %.8f\n ' % (bearing, distance, elevation, WalkToBall.integral))
-    print('Ball!\t Bearing: %f \t Distance: %f\t Elevation: %.8f\t Location: (%d,%d)\n' % (bearing, distance, elevation, ball.imageCenterX, ball.imageCenterY))
-    commands.setHeadPanTilt(0.0, 0.0, 0.1)
+    # print('Ball!\t Bearing: %f \t Distance: %f\t Elevation: %.8f \t Integral: %.8f\n ' % (bearing, distance, elevation, self.integral))
+    print('Ball!\t Bearing: %f \t Distance: %f\t Elevation: %.8f\t Location: (%d,%d)\n' % (bearing, distance, elevation, self.ball.imageCenterX, self.ball.imageCenterY))
+    commands.setHeadPanTilt(bearing, -elevation, 0.1)
+    if abs(self.ball.visionDistance) > 1500.0:
+      commands.setWalkVelocity(0.0, 0.0, -0.5*(np.sign(self.ball.visionBearing)))
+    else:
+      theta_cont = self.kP * self.ball.visionDistance + self.kI * self.theta_integral + self.kD * 20.0 *(self.ball.visionBearing - self.theta_prev)
+      print("Controller input: %.5f\n" % (-theta_cont))
+      commands.setWalkVelocity(0.0, 0.0, -theta_cont)
     # commands.setWalkVelocity(0.2, 0, 0)
 
 class Align(Node):
@@ -150,14 +185,15 @@ class Playing(LoopingStateMachine):
   def setup(self):
     ball = memory.world_objects.getObjPtr(core.WO_BALL)
     goal = memory.world_objects.getObjPtr(core.WO_UNKNOWN_GOAL)
-    stand = GetReady()
+    rdy = GetReady()
     #goToBall = WalkToBall()
     moveHeadLeft = MoveHeadLeft()
     moveHeadRight = MoveHeadRight()
-    sit = pose.Sit()
-    kP = 1.0
-    kI = 1.0
-    kD = 1.0
-    walk = WalkToBall(ball, kP, kI, kD)
-    self.add_transition(stand,C,walk)
+    faceball = FaceBall(ball)
+    # walk = WalkToBall(ball, kP, kI, kD)
+    self.add_transition(rdy,C,moveHeadLeft,C,moveHeadRight)
+    self.add_transition(moveHeadRight,C,moveHeadLeft)
+    self.add_transition(moveHeadLeft,B(ball),faceball)
+    self.add_transition(moveHeadRight,B(ball),faceball)
+    self.add_transition(faceball,B(ball).negation(),moveHeadLeft)
     # self.add_transition(walk,T(3.0),sit)
