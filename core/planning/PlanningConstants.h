b20d51694f86f1f78b666ab15d6b99177b945f16
Tiffany Yang <tiffanyyang@utexas.edu>
2018-12-03 22:59:18 -0600

    Change cell size

--- core/planning/PlanningConstants.h
+++ core/planning/PlanningConstants.h
@@ -1,12 +1,12 @@
 #ifndef PLANNING_CONSTANTS_H
 #define PLANNING_CONSTANTS_H
 
-#define FIELD_WIDTH  (3000)
 #define FIELD_HEIGHT (2500)
-#define CELL_HEIGHT  (200)
-#define CELL_WIDTH   (200)
-#define GRID_HEIGHT  (13)  // ~2480 mm / 200 mm
-#define GRID_WIDTH   (15)  // ~2980 mm / 200 mm
+#define FIELD_WIDTH  (3000)
+#define CELL_HEIGHT  (300)
+#define CELL_WIDTH   (300)
+#define GRID_HEIGHT  (10) //(13)  // ~2480 mm / 200 mm
+#define GRID_WIDTH   (8) //(15)  // ~2980 mm / 200 mm
 #define GRID_SIZE    (GRID_WIDTH * GRID_HEIGHT)
 #define PATH_SIZE    (4 * GRID_SIZE)
 
